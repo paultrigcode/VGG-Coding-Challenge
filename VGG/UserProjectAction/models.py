4ec8437db9e8e379cb1dfe05c45572432bcd1c02
Ajakaye <mailpaulade@gmail.com>
2020-02-29 11:50:56 +0100

    created sqlite3 schema for Users, Projects, and Actions Table.  And made migrations

--- VGG/UserProjectAction/models.py
+++ VGG/UserProjectAction/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	# id , name, description, completed ---> the model fields
+	username = models.CharField(max_length=200, blank=False, unique=True)
+	password = models.CharField(max_length=600, blank=False)
+	
+
+class Project(models.Model):
+	# id , name, description, completed ---> the model fields
+	name = models.CharField(max_length=200, blank=False, unique=True)
+	description = models.CharField(max_length=600, blank=False)
+	completed = models.BooleanField()
+
+
+class Action(models.Model):
+	# id , project-id, description, notes ---> the model fields
+	project_id = models.ForeignKey(Project, blank=False, on_delete=models.CASCADE)
+	description = models.CharField(max_length=600, blank=False)
+	notes = models.CharField(max_length=600, blank=False)
+
+
+
